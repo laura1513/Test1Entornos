73cc77121d3855d2179a0a9988eb7a2d69a4c56c
laura <laura@LAPTOP-MOK29U59>
2022-02-03 18:50:43 +0100

    Creación del proyecto

---
+++ Test1Tema3/src/test1tema3/Test1Tema3.java
@@ -0,0 +1,20 @@
+package test1tema3;
+
+public class Test1Tema3 {
+    /**
+     * Método que multiplica dos números enteros
+     * @param a Primer número emtero
+     * @param b Segundo número entero
+     * @return 
+     */
+public static int multiplica(int a, int b) {
+    return a*b;
+}
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x=3;
+        int y=6;
+        multiplica(x, y);
+    }
+    
+}
