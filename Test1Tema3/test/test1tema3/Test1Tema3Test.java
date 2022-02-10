1b15dd365ee60cd9fcbed279b4e28ec12e208040
laura <laura@LAPTOP-MOK29U59>
2022-02-10 17:43:14 +0100

    He creado la clase test con formato JUnit4

---
+++ Test1Tema3/test/test1tema3/Test1Tema3Test.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package test1tema3;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Ignore;
+
+/**
+ *
+ * @author laura
+ */
+public class Test1Tema3Test {
+    
+    public Test1Tema3Test() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of multiplica method, of class Test1Tema3.
+     */
+    @Test
+    public void testMultiplica() {
+        System.out.println("multiplica");
+        int a = 0;
+        int b = 0;
+        int expResult = 0;
+        int result = Test1Tema3.multiplica(a, b);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of main method, of class Test1Tema3.
+     */
+    @Ignore
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        Test1Tema3.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
