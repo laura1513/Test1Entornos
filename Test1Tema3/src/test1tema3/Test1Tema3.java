b429c8d405789a3dcecd2577157d97c99de14de4
laura <laura@LAPTOP-MOK29U59>
2022-03-03 17:17:17 +0100

    Corregir errores de tipografía

--- Test1Tema3/src/test1tema3/Test1Tema3.java
+++ Test1Tema3/src/test1tema3/Test1Tema3.java
@@ -1,42 +1,49 @@
 package test1tema3;
+
 /**
- * 
+ * Esta clase calculará el factorial de un número o la multiplicación de dos
+ * números.
+ *
  * @author laura
  */
 public class Test1Tema3 {
+
     /**
      * Método que multiplica dos números enteros
+     *
      * @param a Primer número emtero
      * @param b Segundo número entero
-     * @return 
+     * @return Devuelve a*b
      */
-public static int multiplica(int a, int b) {
-    return a*b;
-}
-/**
- * Método par acalcular ell factorial de un número
- * @param num El número que se le introduce
- * @return 
- */
-public static int factorial(int num) {
-    int factorial=num;
-    if(num<0 ) {
-        throw new
-            IllegalArgumentException("Factorial negativo!");
+    public static int multiplica(int a, int b) {
+        return a * b;
     }
-    for (int i = (num-1); i > 1; i--) {
-        factorial=factorial*i;
+
+    /**
+     * Método para calcular el factorial de un número
+     *
+     * @param num El número que se le introduce
+     * @return Devuelve el factorial de num
+     */
+    public static int factorial(int num) {
+        int factorial = num;
+        if (num < 0) {
+            throw new IllegalArgumentException("Factorial negativo!");
+        }
+        for (int i = (num - 1); i > 1; i--) {
+            factorial = factorial * i;
+        }
+        return factorial;
     }
-    return factorial;
-}
+
     public static void main(String[] args) {
         // TODO code application logic here
-        int x=3;
-        int y=6;
+        int x = 3;
+        int y = 6;
         System.out.println(multiplica(x, y));
-        
-        int numero=5;
+
+        int numero = 5;
         System.out.println(factorial(numero));
     }
-    
+
 }
